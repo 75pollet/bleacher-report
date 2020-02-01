a7cce6dbf523ed2e15a93e847700e7a080aa04aa
7_5P <76pollet@gmail.com>
2020-02-01 13:48:27 +0300

    first commit

---
+++ test/bleacher_report_web/views/error_view_test.exs
@@ -0,0 +1,16 @@
+defmodule BleacherReportWeb.ErrorViewTest do
+  use BleacherReportWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.json" do
+    assert render(BleacherReportWeb.ErrorView, "404.json", []) ==
+           %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500.json" do
+    assert render(BleacherReportWeb.ErrorView, "500.json", []) ==
+           %{errors: %{detail: "Internal Server Error"}}
+  end
+end
