6840451ca66f7868f2f37df7e025a19fb204d2af
7_5P <76pollet@gmail.com>
2020-02-01 14:30:11 +0300

    set up code formatting and format the files in the project

--- lib/bleacher_report_web.ex
+++ lib/bleacher_report_web.ex
@@ -29,8 +29,9 @@ defmodule BleacherReportWeb do
 
   def view do
     quote do
-      use Phoenix.View, root: "lib/bleacher_report_web/templates",
-                        namespace: BleacherReportWeb
+      use Phoenix.View,
+        root: "lib/bleacher_report_web/templates",
+        namespace: BleacherReportWeb
 
       # Import convenience functions from controllers
       import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
