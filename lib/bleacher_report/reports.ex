d0bb4a81740d29a4a7d67198dfa6c81fd9b5498b
7_5P <76pollet@gmail.com>
2020-02-02 21:31:13 +0300

    remove all database logic and use only the ets cache to store the user data

--- lib/bleacher_report/reports.ex
+++ lib/bleacher_report/reports.ex
@@ -3,74 +3,60 @@ defmodule BleacherReport.Reports do
     This is where the logics of inserting, updating and
     deleting is done
   """
-  alias BleacherReport.Reports.Report
-  alias BleacherReport.Repo
-  import Ecto.Query
 
   @cache :bleacher_cache
 
   @doc """
-  inserts a report into the database
+  deletes reaction from the cache
   """
-  def insert_report(params) do
-    %Report{}
-    |> Report.changeset(params)
-    |> Repo.insert()
-  end
+  def remove_from_cache(%{"content_id" => content_id, "user_id" => user_id}) do
+    reactions = get_reactions(content_id)
 
-  @doc """
-  delete reaction from the database
-  """
-  def delete_report(report) do
-    Repo.delete(report)
-  end
+    case _check_for_existing_reaction(reactions, user_id) do
+      true ->
+        reactions =
+          Enum.reject(reactions, fn %{"user_id" => user} ->
+            user == user_id
+          end)
 
-  @doc """
-  deletes reaction from the database and updates the cache
-  """
-  def delete_and_update_cache(params) do
-    case Repo.get(Report, params["user_id"]) do
-      nil ->
-        nil
+        ConCache.put(@cache, content_id, reactions)
 
-      report ->
-        delete_report(report)
-
-        ConCache.put(@cache, params["content_id"], _reports_by_content_id(params["content_id"]))
+      false ->
+        :does_not_exist
     end
   end
 
   @doc """
-  checks if the user id exists. if it does not, the report is inserted.
-  if it does, then the report is updated instead
+  insert a reaction into the ets cache
   """
-  def check_user_and_insert_report(params) do
-    case Repo.get(Report, params["user_id"]) do
-      nil -> insert_report(params)
-      _report -> :exists
-    end
-  end
+  def save_to_cache(%{"content_id" => content_id, "user_id" => user_id} = params) do
+    reactions = get_reactions(content_id)
 
-  @doc """
-  insert a reaction into the database then updates the ets cache if the insert
-  is successful
-  """
-  def insert_and_save_to_cache(params) do
-    case check_user_and_insert_report(params) do
-      {:ok, _report} ->
-        ConCache.put(@cache, params["content_id"], _reports_by_content_id(params["content_id"]))
-
-      :exists ->
+    case _check_for_existing_reaction(reactions, user_id) do
+      true ->
         :exists
 
-      _ ->
-        nil
+      false ->
+        new_value = _add_new_value_to_cache(content_id, params)
+
+        ConCache.put(@cache, content_id, new_value)
     end
   end
 
-  defp _get_reports(content_id) do
-    query = from r in Report, where: r.content_id == ^content_id
-    Repo.all(query)
+  defp _add_new_value_to_cache(content_id, params) do
+    case get_reactions(content_id) do
+      nil -> [params]
+      reactions -> reactions |> List.insert_at(-1, params)
+    end
+  end
+
+  defp _check_for_existing_reaction(nil, _user_id) do
+    false
+  end
+
+  defp _check_for_existing_reaction(reactions, user_id) do
+    reactions
+    |> Enum.any?(fn %{"user_id" => user} -> user_id == user end)
   end
 
   @doc """
@@ -78,23 +64,5 @@ defmodule BleacherReport.Reports do
   """
   def get_reactions(content_id) do
     ConCache.get(@cache, content_id)
-  end
-
-  defp _reports_by_content_id(content_id) do
-    content_id
-    |> _get_reports()
-    |> Enum.map(fn report ->
-      _build_cache_params(report)
-    end)
-  end
-
-  defp _build_cache_params(report) do
-    %{
-      "user_id" => report.user_id,
-      "action" => report.action,
-      "content_id" => report.content_id,
-      "reaction_type" => report.reaction_type,
-      "type" => report.type
-    }
   end
 end
--- lib/bleacher_report_web/controllers/bleacher_report_controller.ex
+++ lib/bleacher_report_web/controllers/bleacher_report_controller.ex
@@ -3,15 +3,14 @@ defmodule BleacherReportWeb.BleacherReportController do
   alias BleacherReport.Reports
 
   def reaction(conn, %{"action" => "add"} = params) do
-    case Reports.insert_and_save_to_cache(params) do
+    case Reports.save_to_cache(params) do
       :ok -> send_resp(conn, 200, "ok")
-      nil -> send_resp(conn, 422, "please check your data")
       :exists -> send_resp(conn, 422, "reaction for this user exists")
     end
   end
 
   def reaction(conn, %{"action" => "remove"} = params) do
-    case Reports.delete_and_update_cache(params) do
+    case Reports.remove_from_cache(params) do
       :ok -> send_resp(conn, 200, "ok")
       _ -> send_resp(conn, 422, "reaction doesn't exist")
     end
